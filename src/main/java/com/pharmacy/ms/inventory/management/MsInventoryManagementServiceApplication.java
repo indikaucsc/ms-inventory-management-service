949ec3c42ed3b0afb496a1c0c325693be42965a8
indikaucsc <kamal.indika.n@gmail.com>
2024-09-02 00:23:00 +0800

    Initial commit

---
+++ src/main/java/com/pharmacy/ms/inventory/management/MsInventoryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.ms.inventory.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventoryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventoryManagementServiceApplication.class, args);
+	}
+
+}

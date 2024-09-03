9c01a753ba20e128f0ec1301e5f36918c02aef92
indikaucsc <kamal.indika.n@gmail.com>
2024-09-04 01:15:45 +0800

    create controller

---
+++ src/main/java/com/pharmacy/ms/inventory/management/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.pharmacy.ms.inventory.management.controller;
+
+
+import com.pharmacy.ms.inventory.management.dto.request.ProductRequestDto;
+import com.pharmacy.ms.inventory.management.dto.response.ProductResponseDto;
+import com.pharmacy.ms.inventory.management.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto requestDto) {
+        ProductResponseDto responseDto = productService.createProduct(requestDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+}
